81cfffde0d3910023789758e1e1b966cf6232f09
Geoffrey Harrison <harrisongeo87@gmail.com>
2020-03-27 02:22:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word(string)
   string_array = string.split(" ")
-  string_array = string_array.each{|word| word = word.reverse}
-  
+  string_array = string_array.each do |word| 
+    word = word.reverse
+  end
   new_string = string_array.join(" ")
   new_string
 end
